5015213bf660682f3d4934cb7247b6aee67fb4c6
tukky-14 <bigoonage0104@gmail.com>
2023-03-18 23:59:58 +0900

    位置情報関連の設定を追加

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // 位置情報へのアクセス許可をリクエストするために、以下のコードを追加する
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
